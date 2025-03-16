5e86a3d5e549e8c1aa71f8543f9554f7aaf85adc
rsadhukh <rsadhukh@andrew.cmu.edu>
2025-03-16 01:27:06 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -54,8 +54,7 @@ _deps = [
     "isort>=5.12.0",
     "langdetect",  # Needed for LightEval's extended tasks
     "latex2sympy2_extended>=1.0.6",
-    "liger_kernel==0.5.3",
-    "lighteval @ git+https://github.com/huggingface/lighteval.git@ed084813e0bd12d82a06d9f913291fdbee774905",
+    "liger_kernel==0.5.3", # "lighteval @ git+https://github.com/huggingface/lighteval.git@ed084813e0bd12d82a06d9f913291fdbee774905",
     "math-verify==0.5.2",  # Used for math verification in grpo
     "packaging>=23.0",
     "parameterized>=0.9.0",
