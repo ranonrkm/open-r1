288d428ca85f96b325b85efe94d26f71e2893d7a
rsadhukh <rsadhukh@andrew.cmu.edu>
2025-04-05 16:52:25 -0400

    minor update

--- scripts/sft_7b_local_flame.sh
+++ scripts/sft_7b_local_flame.sh
@@ -8,7 +8,10 @@
 #SBATCH --gres=gpu:8
 #SBATCH --output=logs/sft_7b_local_%A_%a.out
 #SBATCH --error=logs/sft_7b_local_%A_%a.err
-#SBATCH --array=0,2
+#SBATCH --array=0-3
+
+CACHE=/mnt/localssd/rsadhukh
+mkdir -p $CACHE
 
 MODELS=(
     base_ckpt/Qwen/Qwen2.5-Math-7B-Instruct
@@ -16,8 +19,8 @@ MODELS=(
 )
 # NOTE: changes in ckpt names
 ckpt_names=(
-    OpenR1-Qwen-7B-Math-local-ilv_4
-    OpenR1-Qwen-7B-local-ilv_4
+    OpenR1-Qwen-7B-Math-local-layerwise
+    OpenR1-Qwen-7B-local-layerwise
 )
 budgets=(
     2048
@@ -47,19 +50,19 @@ accelerate launch --config_file=recipes/accelerate_configs/zero3.yaml src/open_r
     --per_device_eval_batch_size 1 \
     --per_device_train_batch_size 1 \
     --max_steps -1 \
-    --num_train_epochs 5 \
+    --num_train_epochs 3 \
     --bf16 \
     --do_eval false \
-    --use_liger_kernel \
-    --use_liger \
+    --use_liger_kernel true \
+    --use_liger true \
     --eval_strategy no \
     --gradient_checkpointing \
     --gradient_checkpointing_kwargs '{"use_reentrant": false}' \
     --log_level info \
     --logging_steps 5 \
     --logging_strategy steps \
-    --packing false \
-    --output_dir data/${ckpt_name}-B${budget} \
+    --packing true \
+    --output_dir ${CACHE}/${ckpt_name}-B${budget} \
     --overwrite_output_dir \
     --push_to_hub \
     --report_to wandb \
@@ -69,8 +72,11 @@ accelerate launch --config_file=recipes/accelerate_configs/zero3.yaml src/open_r
     --seed 42 \
     --sparse_training \
     --sparse_attn local \
-    --local ${budget} \
-    --full_attn_layers 0 1 5 10 15 20 25
+    --local ${budget} #\
+    # --full_attn_layers 0 1 5 10 15 20 25
 
     # NOTE: changed from 3 to 5 epochs
     # NOTE: changed from true to false
+
+rm -rf ${CACHE}/${ckpt_name}-B${budget}/checkpoint-*
+mv ${CACHE}/${ckpt_name}-B${budget} data/
